6eedaf6f502338e8891007de97f5430a05dfca04
Haris Shariff <harisms.2022@scis.smu.edu.sg>
2024-02-08 15:39:26 +0800

    inserted main code under server connection while loop

--- firmware.cpp
+++ firmware.cpp
@@ -5,9 +5,6 @@
 #include <ArduinoWebsockets.h>
 #include <WiFi.h>
 
-#define PIN 16
-#define NUMPIXELS 3
-
 using namespace websockets;
 // Create a WebSocket client instance
 WebsocketsClient client;
@@ -16,6 +13,8 @@ Adafruit_NeoPixel strip = Adafruit_NeoPixel(NUMPIXELS, PIN, NEO_GRB + NEO_KHZ800
 void setup() {
     Serial.begin(115200);
     colorWipe(strip.Color(255, 0, 0), 50); // Red
+    pinMode(DOOR_PIN, OUTPUT);
+    digitalWrite(DOOR_PIN, HIGH);
 
     // Connect to Wi-Fi
     WiFi.begin(WIFI_SSID, PASSWORD);
@@ -32,58 +31,62 @@ void setup() {
     while (!client.connect(SERVERADDRESS, PORT, "/")) {
         Serial.println("Failed to connect to WebSocket server. Retrying...");
         delay(1000);
-    }
 
-    Serial.println("Connected to WebSocket server.");
-    colorWipe(strip.Color(173, 216, 230), 50); // Light Blue connected
+        Serial.println("Connected to WebSocket server.");
+        colorWipe(strip.Color(173, 216, 230), 50); // Light Blue connected
 
-    // Register a callback function to handle incoming messages
-    client.onMessage([](WebsocketsMessage message) {
-        Serial.println("Received message: " + message.data());
-        colorWipe(strip.Color(0, 0, 255), 2000); // Blue message received
-        colorWipe(strip.Color(255, 125, 0), 50); // Orange connected, waiting for instruction
+        // Register a callback function to handle incoming messages
+        client.onMessage([](WebsocketsMessage message) {
+            Serial.println("Received message: " + message.data());
+            colorWipe(strip.Color(0, 0, 255), 2000); // Blue message received
+            colorWipe(strip.Color(255, 125, 0), 50); // Orange connected, waiting for instruction
 
-        // Parse JSON data here
-        DynamicJsonDocument doc(1024); // Adjust size according to JSON size
-        DeserializationError error = deserializeJson(doc, message.data());
+            // Parse JSON data here
+            DynamicJsonDocument doc(1024); // Adjust size according to JSON size
+            DeserializationError error = deserializeJson(doc, message.data());
 
-        if (error) {
-            Serial.print(F("deserializeJson() failed: "));
-            Serial.println(error.f_str());
-            return;
-        }
-
-        // Extract from JSON
-        const char *esp32ID = doc["esp32Code"];
-        const char *pw = doc["password"];
-        const char *instruction = doc["instruction"];
-        colorWipe(strip.Color(75, 0, 130), 1000); // Purple JSON parser success
-        Serial.println(esp32ID);
-        Serial.println(pw);
-        Serial.println(instruction);
-
-        // Check if receiving ID is the same as ESP's ID
-        // Hash receiving password and hash ESP's password
-        // Compare and check if correct:
-        // Only if when both correct read instruction and execute
-
-        if (strcmp(ESPID, esp32ID)) {
-            char ESPPWHash[65];
-            hashPassword(ESPPW, ESPPWHash);
-
-            char pwHash[65];
-            hashPassword(pw, pwHash);
-
-            // Compare hash
-            if (strcmp(ESPPWHash, pwHash) == 1) {
-                // INSERT UNLOCK CODE HERE
-                colorWipe(strip.Color(0, 255, 0), 2000); // GREEN DOOR UNLOCK
-            } else {
-                // DON'T DO ANYTHING REJECT
-                colorWipe(strip.Color(255, 0, 0), 2000); // RED REJECT
+            if (error) {
+                Serial.print(F("deserializeJson() failed: "));
+                Serial.println(error.f_str());
+                return;
             }
-        }
-    });
+
+            // Extract from JSON
+            const char *esp32ID = doc["esp32Code"];
+            const char *pw = doc["password"];
+            const char *instruction = doc["instruction"];
+            colorWipe(strip.Color(75, 0, 130), 1000); // Purple JSON parser success
+            Serial.println(esp32ID);
+            Serial.println(pw);
+            Serial.println(instruction);
+
+            // Check if receiving ID is the same as ESP's ID
+            // Hash receiving password and hash ESP's password
+            // Compare and check if correct:
+            // Only if when both correct read instruction and execute
+
+            if (strcmp(ESPID, esp32ID) == 0) { // ID is correct
+                char ESPPWHash[65];
+                hashPassword(ESPPW, ESPPWHash);
+
+                char pwHash[65];
+                hashPassword(pw, pwHash);
+
+                // Compare hash
+                if (strcmp(ESPPWHash, pwHash) == 0) {
+                    // INSERT UNLOCK CODE HERE
+                    Serial.println("UNLOCK");
+                    digitalWrite(DOOR_PIN, LOW);
+                    colorWipe(strip.Color(0, 255, 0), 2000); // GREEN DOOR UNLOCK
+                } else {
+                    // DON'T DO ANYTHING REJECT
+                    colorWipe(strip.Color(255, 0, 0), 2000); // RED REJECT
+                }
+                digitalWrite(DOOR_PIN, HIGH);
+                colorWipe(strip.Color(0, 0, 255), 2000);
+            }
+        });
+    }
 }
 
 void loop() {
